6228addcb7a025bd01f56da675d90931ad8d5128
Paulos-se <llcoolp@live.co.uk>
2022-07-04 13:45:11 +0100

    if accepted this commit will retrieve topics as requested by the api

--- controllers/controllers.news.js
+++ controllers/controllers.news.js
@@ -2,7 +2,6 @@ const { fetchTopics } = require("../models/models.news");
 
 
 exports.getTopics = (req, res, next) => {
-    console.log("im in controller")
     fetchTopics().then(( result) => {
         console.log(result);
         res.status(200).send({ topics:result });
