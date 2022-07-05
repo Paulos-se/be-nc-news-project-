e7437a5d74c4c3a4b62af3ed4c0adef5b6e55d55
Paulos Ghirmai <54372405+Paulos-se@users.noreply.github.com>
2022-07-05 10:28:28 +0100

    Revert "Ncnews 2"

--- controllers/controllers.news.js
+++
@@ -1,9 +0,0 @@
-const { fetchTopics } = require("../models/models.news");
-
-
-exports.getTopics = (req, res, next) => {
-    fetchTopics().then(( result) => {
-        console.log(result);
-        res.status(200).send({ topics:result });
-    })
-}
--- models/models.news.js
+++
@@ -1,9 +0,0 @@
-const db = require('../db/connection');
-
-
-exports.fetchTopics = () => {
-    console.log("im in the fetch");
-    return db.query(`SELECT * FROM topics;`).then(({ rows }) => {
-        return rows;
-    })
-}
