2ff8d2853c82bca981ae2d048141680073638675
Paulos-se <llcoolp@live.co.uk>
2022-07-05 10:27:29 +0100

    test

--- controllers/controllers.news.js
+++ controllers/controllers.news.js
@@ -2,8 +2,7 @@ const { fetchTopics } = require("../models/models.news");
 
 
 exports.getTopics = (req, res, next) => {
-    fetchTopics().then(( result) => {
-        console.log(result);
-        res.status(200).send({ topics:result });
+    fetchTopics().then(( topics) => {
+        res.status(200).send({ topics});
     })
 }
