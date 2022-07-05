d361ac28621bcdce4e45969a3ec9268f285fd0c2
Paulos-se <llcoolp@live.co.uk>
2022-07-05 17:36:18 +0100

    if accepted this commit will complete task 6

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
-const app = (express());
-const { getTopics, getArticleById, patchArticleVote,getUsers } = require("./controllers/controllers.news");
+const app = express();
+const {
+  getTopics,
+  getArticleById,
+  patchArticleVote,
+  getUsers,
+} = require("./controllers/controllers.news");
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
@@ -9,28 +14,21 @@ app.patch("/api/articles/:article_id", patchArticleVote);
 app.get("/api/users", getUsers);
 
 app.use("*", (req, res) => {
-    res.status(404).send({message:"not found"})
-})
+  res.status(404).send({ message: "not found" });
+});
 
-app.use((err, req, res, next)=>{
-// console.log(err)
-    if (err.status && err.message) {
-        res.status(err.status).send({message:err.message})
-    } else {
-        next(err);
-    }
-})
-
-
+app.use((err, req, res, next) => {
+  // console.log(err)
+  if (err.status && err.message) {
+    res.status(err.status).send({ message: err.message });
+  } else {
+    next(err);
+  }
+});
 
 app.use((err, req, res) => {
-    console.log(err);
-    res.status(500).send({ message: "server error" });
-})
-
-
-
-
-
+  console.log(err);
+  res.status(500).send({ message: "server error" });
+});
 
 module.exports = app;
--- models/models.news.js
+++ models/models.news.js
@@ -1,57 +1,60 @@
-const db = require('../db/connection');
+const db = require("../db/connection");
 
 exports.fetchTopics = () => {
-    return db.query(`SELECT * FROM topics;`).then(({ rows }) => {
-        return rows;
-    })
-}
+  return db.query(`SELECT * FROM topics;`).then(({ rows }) => {
+    return rows;
+  });
+};
 
 exports.fetchArticleById = (id) => {
-    
-
-    if (isNaN(parseInt(id))) {
-        return Promise.reject({
-            status: 400,
-            message: `invalid id`
-        });
-    } else {
-
-        return db.query(`SELECT articles.author, articles.title, articles.article_id,articles.body,articles.topic, articles.created_at,articles.votes FROM articles WHERE articles.article_id=$1`, [id]).then(({ rows, rowCount }) => {
-            if (rowCount === 0) {
-                return Promise.reject({
-                    status: 404,
-                    message: `article ${id} not found.`
-                });
-            }
-            return rows[0];
-        })
-    
-    }
-}
+  if (isNaN(parseInt(id))) {
+    return Promise.reject({
+      status: 400,
+      message: `invalid id`,
+    });
+  } else {
+    return db
+      .query(
+        `SELECT articles.author, articles.title, articles.article_id,articles.body,articles.topic, articles.created_at,articles.votes FROM articles WHERE articles.article_id=$1`,
+        [id]
+      )
+      .then(({ rows, rowCount }) => {
+        if (rowCount === 0) {
+          return Promise.reject({
+            status: 404,
+            message: `article ${id} not found.`,
+          });
+        }
+        return rows[0];
+      });
+  }
+};
 
 exports.updateArticleVote = (vote, id) => {
-    if (!vote) {
-        return Promise.reject({
-            status: 400,
-            message:"bad formatted update"
-        })
-    }
-    else if (isNaN(parseInt(vote))) {
-       
-        return Promise.reject({
-            status: 400,
-            message:"invalid update"
-        });
-    } else {
-        
-        return db.query(`UPDATE articles SET votes=votes+${vote} WHERE article_id=$1 RETURNING *`, [id]).then(({ rows }) => {
-            return rows[0].votes;
-        })
-    }
-}
+  if (!vote) {
+    return Promise.reject({
+      status: 400,
+      message: "bad formatted update",
+    });
+  } else if (isNaN(parseInt(vote))) {
+    return Promise.reject({
+      status: 400,
+      message: "invalid update",
+    });
+  } else {
+    return db
+      .query(
+        `UPDATE articles SET votes=votes+${vote} WHERE article_id=$1 RETURNING *`,
+        [id]
+      )
+      .then(({ rows }) => {
+        return rows[0].votes;
+      });
+  }
+};
 
 exports.fetchUsers = () => {
-    return db.query(`SELECT * FROM users`).then(({ rows }) => {
-        return rows;
-    })
-}
+  return db.query(`SELECT * FROM users`).then(({ rows }) => {
+    return rows;
+  });
+};
